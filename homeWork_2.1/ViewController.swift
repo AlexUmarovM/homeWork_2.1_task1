188d089cb35731474d188a15035fda638c387163
Alex Umarov <umarov.iosdev@gmail.com>
2020-05-19 20:49:40 +0300

    Initial Commit

---
+++ homeWork_2.1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  homeWork_2.1
+//
+//  Created by Александр Умаров on 19.05.2020.
+//  Copyright © 2020 Александр Умаров. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
